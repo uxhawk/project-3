3149fea3de19ebc0c9258f4c01b0ea148eea834f
uxhawk <uxhawk@gmail.com>
2020-06-25 16:58:12 -0400

    setting up a working front end and back end structure that we can use as a starting point

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,17 +1,18 @@
 // eslint-disable-next-line new-cap
 const router = require('express').Router();
-const booksController = require('../../controllers/booksController');
+// const booksController = require('../../controllers
+// userFinancialsController');
 
 // Matches with "/api/books"
-router.route('/')
-    .get(booksController.findAll)
-    .post(booksController.create);
+// router.route('/')
+//     .get(booksController.findAll)
+//     .post(booksController.create);
 
 // Matches with "/api/books/:id"
-router
-    .route('/:id')
-    .get(booksController.findById)
-    .put(booksController.update)
-    .delete(booksController.remove);
+// router
+// .route('/:id')
+// .get(booksController.findById)
+// .put(booksController.update)
+// .delete(booksController.remove);
 
 module.exports = router;
