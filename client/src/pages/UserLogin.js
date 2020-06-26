e7113cc2786eb91bdb600cb2a1a21e6e3bb6b7f6
uxhawk <uxhawk@gmail.com>
2020-06-26 17:26:17 -0400

    removed google sign in button

--- client/src/pages/UserLogin.js
+++ client/src/pages/UserLogin.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const UserLogin = () => {
+    function handleBtnClick() {
+        console.log('hello');
+    }
     return (
         <div>
             Welcome to Tile Master Finances. Sign in to get started.
-            <div id="my-signin2"></div>
+            <div id="my-signin2" onClick={() => {handleBtnClick()}}></div>
         </div>
     );
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable new-cap */
 const router = require('express').Router();
-const bookRoutes = require('./books');
+// const bookRoutes = require('./books');
+const authRoutes = require('./auth-routes');
 
 // Book routes
-router.use('/books', bookRoutes);
+// router.use('/books', bookRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
