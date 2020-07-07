fac0599ecefe25ebaaa14b4e0ba69ae0c54a74ba
uxhawk <uxhawk@gmail.com>
2020-07-07 16:15:31 -0400

    added the outline for the form

--- client/src/pages/Transaction.js
+++ client/src/pages/Transaction.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useStoreContext } from '../utils/GlobalState';
 import { Redirect, useHistory } from 'react-router-dom';
 import API from '../utils/API';
@@ -8,6 +8,26 @@ const Transaction = () => {
     const [loading, setLoading] = useState(true);
     const [state, dispatch] = useStoreContext();
     let history = useHistory();
+
+    // ref for inputs from the form
+    const amountRef = useRef();
+    const categoryRef = useRef();
+    const dateRef = useRef();
+    const detailsRef = useRef();
+
+    // function to submit form 
+    function submitForm(event) {
+        event.preventDefault();
+
+        const transaction = {
+            amount: amountRef.current.value,
+            date: dateRef.current.value,
+            category: categoryRef.current.value,
+            details: detailsRef.current.value
+        }
+        console.log(transaction);
+    }
+
 
     function handleNavClick(event) {
         const destination = event.target.getAttribute('nav-value');
@@ -40,10 +60,34 @@ const Transaction = () => {
                 state.user ? 
                     <div>
                         Transaction
-                        <button className="btn btn-info" onClick={(event) => {handleNavClick(event)}} nav-value="stock-market">Stock Market</button>
-                        <button className="btn btn-info" onClick={(event) => {handleNavClick(event)}} nav-value="goals">Goals</button>
-                        <button className="btn btn-info" onClick={(event) => {handleNavClick(event)}} nav-value="transaction">Add Transactions</button>
-                        <button className="btn btn-info" onClick={(event) => {handleNavClick(event)}} nav-value="dashboard">Dashboard</button>
+                        <div className="row">
+                            <div className="col-lg-4 offset-lg-4">
+                                <div className="card">
+                                    <div className="card-body">
+                                        Add transactions and payments to track your budget
+                                        <form>
+                                            <input type="number" id="transactionAmount" className="form-control" placeholder="$$$" required="" autoFocus="" ref={amountRef}/>
+                                            <input type="number" id="transactionAmount" className="form-control" placeholder="Transaction Date" required="" autoFocus="" ref={dateRef}/>
+                                            
+                                            <select id="category" className="form-control" ref={categoryRef}>
+                                                <option value="income">Income</option>
+                                                <option value="bar/restaurant">Bar/Restaurant</option>
+                                                <option value="travel">Travel</option>
+                                                <option value="groceries">Grocery Store</option>
+                                                <option value="utilities">Bill</option>
+                                                <option value="mortgage/rent"></option>
+                                            </select> 
+                                            <textarea className="form-control" aria-label="With textarea" placeholder="Transaction notes and details" ref={detailsRef}></textarea>
+                                            <button className="btn btn-primary" type="submit" onClick={(event) => {submitForm(event)}}>Add Transaction</button>
+                                          
+                                            
+
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>  : 
                     <Redirect to="/login" />
                 }
@@ -52,6 +96,11 @@ const Transaction = () => {
         </div>
     );
 };
+
+{/* <button className="btn btn-info" onClick={(event) => {handleNavClick(event)}} nav-value="stock-market">Stock Market</button>
+<button className="btn btn-info" onClick={(event) => {handleNavClick(event)}} nav-value="goals">Goals</button>
+<button className="btn btn-info" onClick={(event) => {handleNavClick(event)}} nav-value="transaction">Add Transactions</button>
+<button className="btn btn-info" onClick={(event) => {handleNavClick(event)}} nav-value="dashboard">Dashboard</button> */}
 
 export default Transaction;
 
