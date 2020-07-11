2107631be4918c43bb2969d9c108c5f494b0e675
Spunkyfrog93 <60330400+Spunkyfrog93@users.noreply.github.com>
2020-07-11 10:20:11 -0400

    update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -27,5 +27,21 @@ export default {
   getCurrentStockPrice: function(symbol) {
     return axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=1BGUWMMYKV1JPI0R`);
   },
-  
+  // Gets all posts
+  getPosts: function() {
+    return axios.get("/api/posts");
+  },
+  // Gets the post with the given id
+  getPost: function(id) {
+    return axios.get("/api/posts/" + id);
+  },
+  // Deletes the post with the given id
+  deletePost: function(id) {
+    return axios.delete("/api/posts/" + id);
+  },
+  // Saves a post to the database
+  savePost: function(postData) {
+    return axios.post("/api/posts", postData);
+  }
 };
+
