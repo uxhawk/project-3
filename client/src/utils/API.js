6d0bebb763673699879f15f9dcf5ec2993470d89
uxhawk <uxhawk@gmail.com>
2020-07-15 20:39:07 -0400

    working stock market page

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -17,13 +17,13 @@ export default {
   },
   getSymbols: function() {
     console.log(process.env.REACT_APP_FINNHUB_API);
-    return axios.get(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${process.env.REACT_APP_FINNHUB_API}`);
+    return axios.get(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=bpul0uvrh5rd45tk1jrg`);
   },
   getMutualFundSymbols: function() {
-    return axios.get(`https://finnhub.io/api/v1/stock/symbol?exchange=mutualFund&token=${process.env.REACT_APP_FINNHUB_API}`);
+    return axios.get(`https://finnhub.io/api/v1/stock/symbol?exchange=mutualFund&token=bpul0uvrh5rd45tk1jrg`);
   },
   getIndexPrices: function(index) {
-    return axios.get(`https://finnhub.io/api/v1/quote?symbol=^${index}&token=${process.env.REACT_APP_FINNHUB_API}`);
+    return axios.get(`https://finnhub.io/api/v1/quote?symbol=^${index}&token=bpul0uvrh5rd45tk1jrg`);
   },
   getCurrentStockPrice: function(symbol) {
     return axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=1BGUWMMYKV1JPI0R`);
